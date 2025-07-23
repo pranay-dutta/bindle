0cf0795c8b165eaca806bfd6a82bc168f60bc5c7
Pranay Dutta <rick15176@gmail.com>
2025-07-23 21:26:51 +0530

    create nyt client

---
+++ src/services/nytClient.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+const { VITE_NYT_API, VITE_NYT_URL } = import.meta.env;
+
+const client = axios.create({
+  baseURL: VITE_NYT_URL,
+  params: {
+    "api-key": VITE_NYT_API,
+  },
+});
+
+class NYTClient<T> {
+  private endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  get = async () => {
+    return client.get<T>(this.endpoint).then((res) => res.data);
+  };
+}
+export default NYTClient;
