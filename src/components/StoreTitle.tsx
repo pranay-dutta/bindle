20c97313594960348d00b84219f8be2556392582
Pranay Dutta <rick15176@gmail.com>
2025-07-26 01:13:45 +0530

    refactor: store title

--- src/components/StoreTitle.tsx
+++ src/components/StoreTitle.tsx
@@ -1,8 +1,10 @@
 import { Heading } from "@chakra-ui/react";
 import { STORE_TITLE } from "@/constants";
+import { useNavigate } from "react-router";
 
 const StoreTitle = () => {
-  return <Heading>{STORE_TITLE}</Heading>;
+  const navigate = useNavigate();
+  return <Heading onClick={() => navigate("/")}>{STORE_TITLE}</Heading>;
 };
 
 export default StoreTitle;
--- tests/components/StoreTitle.test.tsx
+++ tests/components/StoreTitle.test.tsx
@@ -3,7 +3,6 @@ import { screen, setup } from "../renderSetup";
 import StoreTitle from "@/components/StoreTitle";
 import { STORE_TITLE } from "@/constants";
 
-
 describe("StoreTitle", () => {
   it("should render store title", () => {
     setup(<StoreTitle />);
@@ -12,5 +11,13 @@ describe("StoreTitle", () => {
 
     expect(storeTitle).toBeInTheDocument();
     expect(storeTitle).toHaveTextContent(STORE_TITLE);
+  });
+  it("should navigate to homepage on click", () => {
+    const { user } = setup(<StoreTitle />);
+
+    const storeTitle = screen.getByText(STORE_TITLE);
+    user.click(storeTitle);
+
+    expect(window.location.pathname).toBe("/");
   });
 });
