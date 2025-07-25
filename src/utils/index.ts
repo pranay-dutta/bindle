b6c77ef1362cb99f205f2c4d8c7ed18b1463aab2
Pranay Dutta <rick15176@gmail.com>
2025-07-25 20:44:44 +0530

    add toKebabCase utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export const toNormalCase = (inputString: string) => {
   return inputString.toLowerCase().replace(/(^|\s)\w/g, (c) => c.toUpperCase());
 };
+
+export const toKebabCase = (inputString: string) => {
+  return inputString.toLowerCase().replace(/\s+/g, "-");
+};
