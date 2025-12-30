bd6787173e6b4056b68d5d50b171dc7b0f443875
Pranay Dutta <rick15176@gmail.com>
2025-12-30 21:05:42 +0530

    create: ebook, recommendation and sale page

---
+++ src/pages/SalePage.tsx
@@ -0,0 +1,39 @@
+import OpenLibBook from "@/components/open-library/OpenLibBook";
+import OpenLibBookSkeleton from "@/components/open-library/OpenLibBookSkeleton";
+import Pagination from "@/components/Pagination";
+import useOpenLibSubjectList from "@/hooks/open-library/useOpenLibSubjectList";
+import { Box, Group, Heading, Grid } from "@chakra-ui/react";
+import { BsStars } from "react-icons/bs";
+import { useSearchParams } from "react-router";
+
+const SalePage = () => {
+  const PAGE_SIZE = 14;
+  const subject = "Programming";
+  const [searchParams] = useSearchParams();
+  const pageNumber = parseInt(searchParams.get("page") || "1") - 1;
+  const { data, isLoading } = useOpenLibSubjectList(subject, pageNumber, PAGE_SIZE);
+
+  return (
+    <Box p={4}>
+      <Group display={"flex"} flexDirection="column" gap={4} alignItems="center">
+        <Heading my={4}>
+          Our Recommendation
+          <BsStars style={{ display: "inline", marginLeft: "4px", color: "orangered" }} />
+        </Heading>
+
+        <Grid alignContent="start" templateColumns="repeat(7, 1fr)" gap={4}>
+          {isLoading
+            ? Array.from({ length: PAGE_SIZE }, (_, index) => <OpenLibBookSkeleton key={index} />)
+            : data?.works.map((book) => <OpenLibBook key={book.key} openLibBook={book} />)}
+        </Grid>
+
+        {/* Pagination Component */}
+        <Box mx="auto" mt={5} w="fit-content">
+          <Pagination pageSize={PAGE_SIZE} totalCount={data?.work_count || 0} />
+        </Box>
+      </Group>
+    </Box>
+  );
+};
+
+export default SalePage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./Layout/Layout";
 import Homepage from "./pages/Homepage";
-import BookDetailPage from "./pages/BookDetail";
+import BookDetailPage from "./pages/BookDetailPage";
 import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import SearchResultsPage from "./pages/SearchResultsPage";
+import CategoryPageContainer from "./pages/categorypage/container/CategoryPageContainer";
+import RecommendationPage from "./pages/RecommendationPage";
+import NewReleasePage from "./pages/NewReleasePage";
+import EbookPage from "./pages/EbookPage";
+import SalePage from "./pages/SalePage";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +27,34 @@ const router = createBrowserRouter([
       {
         path: "cart",
         element: <CartPage />
+      },
+      {
+        path: "checkout",
+        element: <CheckoutPage />
+      },
+      {
+        path: "search",
+        element: <SearchResultsPage />
+      },
+      {
+        path: "categories/:category",
+        element: <CategoryPageContainer />
+      },
+      {
+        path: "recommendations",
+        element: <RecommendationPage />
+      },
+      {
+        path: "new-releases",
+        element: <NewReleasePage />
+      },
+      {
+        path: "ebooks",
+        element: <EbookPage />
+      },
+      {
+        path: "sale",
+        element: <SalePage />
       }
     ]
   }
