324b2b84cfe28b5fede6d5e944243f34f911fc94
Pranay Dutta <rick15176@gmail.com>
2026-01-04 00:09:29 +0530

    refactor: cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -3,43 +3,84 @@ import type { NytBook } from "@/interfaces/new-york-times/NytBook";
 import type { SearchedBook } from "@/interfaces/open-library/OLSearch";
 import useCartStore from "@/store/useCartStore";
 import { getOLCoverUrls, isNytBook, toNormalCase } from "@/utils";
-import { Box, Button, Flex, Heading, HStack, Image, Input, Span, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Image,
+  Input,
+  Span,
+  Text
+} from "@chakra-ui/react";
 import { FiTrash } from "react-icons/fi";
 
 const CartPage = () => {
   const cart = useCartStore((state) => state.cart);
   const getQty = useCartStore((state) => state.getQty);
-  const totalCartValue = [...cart.keys()].reduce((acc, book) => acc + parseInt(book.price) * getQty(book), 0);
+  const totalCartValue = [...cart.keys()].reduce(
+    (acc, book) => acc + parseInt(book.price) * getQty(book),
+    0
+  );
+  const cartItems = [...cart.keys()];
 
-  //TODO: implement cart is empty logic
   return (
-    <Flex gap={5}>
-      {/* My Shopping cart */}
-      <Flex direction="column" gap={5} width="1/2">
-        <Heading fontSize="2xl">My Shopping Cart</Heading>
-        {[...cart.keys()].map((book) => (
-          <ShoppingBook key={book.title} book={book} />
-        ))}
-      </Flex>
-
-      {/* My Orders */}
-      <Flex direction="column" width="1/2">
-        <Heading fontSize="2xl">My Order</Heading>
-        {[...cart.keys()].map((book) => (
-          <Flex direction="row" justifyContent="space-between" key={book.title}>
-            <Text>
-              {getQty(book)} x {toNormalCase(book.title)}
-            </Text>
-            <Text>$ {parseInt(book.price) * getQty(book)}</Text>
+    <Box h={"65vh"}>
+      <Flex gap={10} maxH="100%" py={5}>
+        {/* My Shopping Cart */}
+        <Flex flexDirection="column" width="3/5">
+          <Text mb={3} fontSize="3xl">
+            My Shopping Cart
+          </Text>
+          <Flex overflowY="auto" direction="column" gap={2} pr={4}>
+            {cartItems.map((book, index) => (
+              <Box
+                key={book.title}
+                borderBottom={
+                  index === cartItems.length - 1 ? "" : "1px solid #ddd"
+                }
+                py={1}
+              >
+                <ShoppingBook book={book} />
+              </Box>
+            ))}
           </Flex>
-        ))}
+        </Flex>
 
-        <Text fontSize="xl" fontWeight="bold">
-          Total: ${totalCartValue}
-        </Text>
-        <Button bg="orange.500">Order and pay {totalCartValue}</Button>
+        {/* My Orders */}
+        <Flex
+          direction="column"
+          gap={4}
+          width="2/5"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Text mb={3} fontSize="3xl">
+              My Orders
+            </Text>
+            {[...cart.keys()].map((book) => (
+              <Flex
+                direction="row"
+                justifyContent="space-between"
+                key={book.title}
+              >
+                <Text truncate>
+                  {getQty(book)} x {toNormalCase(book.title)}
+                </Text>
+                <Text textWrap="nowrap">
+                  $ {parseInt(book.price) * getQty(book)}
+                </Text>
+              </Flex>
+            ))}
+
+            <Text fontSize="xl" fontWeight="bold">
+              Total: ${totalCartValue}
+            </Text>
+          </Box>
+          <Button bg="orange.500">Order and pay {totalCartValue}</Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </Box>
   );
 };
 
@@ -48,8 +89,8 @@ const ShoppingBook = ({ book }: { book: NytBook | SearchedBook }) => {
   const setQty = useCartStore((state) => state.setQty);
   const getQty = useCartStore((state) => state.getQty);
 
-  const height = 180;
-  const width = 120;
+  const height = 150;
+  const width = 100;
 
   const handleQtyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newQty = Number(e.target.value);
@@ -65,7 +106,7 @@ const ShoppingBook = ({ book }: { book: NytBook | SearchedBook }) => {
   const { src, onError } = useImageFallback(urls);
 
   return (
-    <Flex direction="row" gap={4} width="100%" borderBottom="1px solid black" pb={5}>
+    <Flex direction="row" gap={4} width="100%" py={2}>
       {/* Book Cover */}
       <Box minW={width} h={height} w={width} minH={height}>
         {isNyt ? (
@@ -77,20 +118,30 @@ const ShoppingBook = ({ book }: { book: NytBook | SearchedBook }) => {
 
       {/* Book Details */}
       <Flex direction="column" width="full">
-        <HStack height="full" justifyContent="space-between" alignItems="flex-start">
+        <HStack
+          height="full"
+          justifyContent="space-between"
+          alignItems="flex-start"
+        >
           <Box>
-            <Text>{toNormalCase(book.title)}</Text>
-            <Text>{isNyt && book.author}</Text>
+            <Text fontSize="medium" fontWeight="medium">
+              {toNormalCase(book.title)}
+            </Text>
+            <Text fontSize="sm" color="gray.500">
+              {isNyt && book.author}
+            </Text>
           </Box>
 
-          <Text onClick={() => removeItem(book)}>
+          <Text onClick={() => removeItem(book)} cursor={"pointer"}>
             Remove
             <FiTrash style={{ display: "inline-block", marginLeft: "4px" }} />
           </Text>
         </HStack>
 
         <HStack justifyContent="space-between" alignItems="center">
-          <Text>{book.price}</Text>
+          <Text color="orange.600" fontWeight="medium">
+            $ {book.price}
+          </Text>
           <Text>
             <Span mr={2}>Qty</Span>
             <Input
