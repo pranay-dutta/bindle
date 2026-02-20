7e1bda31627d12ef86c62e1aa0869aabbc158abf
Pranay Dutta <rick15176@gmail.com>
2026-02-20 22:26:19 +0530

    create: add hooks

--- src/hooks/new-york-times/useNytBookList.ts
+++ src/hooks/new-york-times/useNytBookList.ts
@@ -1,16 +1,28 @@
 import type { List } from "@/interfaces/new-york-times/List";
 import type { ListNames } from "@/interfaces/new-york-times/ListNames";
-import createNytClient from "@/services/nytClient";
+import createBackendClient from "@/services/backendClient";
+// import createNytClient from "@/services/nytClient";
 import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 
 const useNytBookList = (listName: ListNames) => {
-  const nytClient = createNytClient<List>(`/current/${listName}.json`);
+  // const nytClient = createNytClient<List>(`/current/${listName}.json`);
+  const client = createBackendClient<List>(`/books/${listName}`);
+  const getTempData = async () => {
+    return client.getNytAll();
+  };
 
   return useQuery({
     queryKey: ["list", listName],
-    queryFn: nytClient.getAll,
+    queryFn: () => getTempData(),
     staleTime: ms("2h"),
+    select: (data) => {
+      const books = data.books.map((book) => {
+        book.price = Math.floor(Math.random() * 100).toString();
+        return book;
+      });
+      return { data, books: books };
+    }
   });
 };
 export default useNytBookList;
---
+++ src/services/backendClient.ts
@@ -0,0 +1,26 @@
+import type { FetchResponse } from "@/interfaces/new-york-times/FetchResponse";
+import axios, { type AxiosRequestConfig } from "axios";
+const { VITE_BACKEND_URL } = import.meta.env;
+
+const client = axios.create({
+  baseURL: VITE_BACKEND_URL
+});
+
+class BackendClient<T> {
+  private endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  get = async (params?: AxiosRequestConfig) => {
+    return client.get<T>(this.endpoint, params).then((res) => res.data);
+  };
+  getNytAll = async () => {
+    return client.get<FetchResponse<T>>(this.endpoint).then((res) => res.data.results);
+  };
+}
+const createBackendClient = <T>(endpoint: string) => {
+  return new BackendClient<T>(endpoint);
+};
+export default createBackendClient;
