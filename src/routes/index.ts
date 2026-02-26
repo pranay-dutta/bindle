49e1366f450cbc5a6ceaa912e26aec13520c0d49
Pranay Dutta <rick15176@gmail.com>
2026-02-27 02:07:17 +0530

    create: add prettier configuration and workspace settings

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import bookRoutes from "./books";
 import subjectRoutes from "./subjects";
+import userRoutes from "./user";
+import orderRoutes from "./order";
 import { Router } from "express";
 
 const router = Router();
 
 router.use("/books", bookRoutes);
 router.use("/subjects", subjectRoutes);
+router.use("/user", userRoutes);
+router.use("/order", orderRoutes);
 
 export default router;
