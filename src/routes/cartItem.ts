3dff6890f0d888f247614aa571975ae976f1d490
Pranay Dutta <rick15176@gmail.com>
2026-03-03 15:27:35 +0530

    refactor: use validate user helper and add increase and decrease route

--- src/routes/cartItem.ts
+++ src/routes/cartItem.ts
@@ -1,28 +1,48 @@
-import requireAuth from "../middlewares/requireAuth"
 import { Router } from "express"
 import { prisma } from "../../lib/prisma"
+import { validateUser } from "../utils/auth"
 
 const router = Router()
 
-//Update a single cart item
-router.put("/:itemId", requireAuth, async (req, res) => {
-  const { itemId } = req.params
-  const { quantity } = req.body
+//Create a single cart item
+router.post("/add", async (req, res) => {
+  const user = await validateUser(req)
+  if (!user) return res.status(401).json({ error: "Unauthorized" })
 
-  if (quantity < 1) {
-    return res.status(400).json({ error: "Quantity must be at least 1" })
-  }
-
-  const updateItem = await prisma.cartItem.update({
-    where: { id: itemId },
-    data: { quantity }
+  //Create of fetch cart of current user
+  const cart = await prisma.cart.upsert({
+    where: { userId: user.id },
+    create: { userId: user.id },
+    update: {}
   })
 
-  res.status(200).json({ message: "Cart item updated successfully", updateItem })
+  const existingItem = await prisma.cartItem.findFirst({
+    where: { cartId: cart.id, bookTitle: req.body.bookTitle }
+  })
+
+  if (existingItem) {
+    const response = await prisma.cartItem.update({
+      where: { id: existingItem.id },
+      data: { quantity: existingItem.quantity + req.body.quantity }
+    })
+
+    return res.status(200).json({ message: "Cart item updated successfully", response })
+  } else {
+    const response = await prisma.cartItem.create({
+      data: {
+        cartId: cart.id,
+        bookTitle: req.body.bookTitle,
+        image: req.body.image,
+        price: req.body.price,
+        quantity: req.body.quantity
+      }
+    })
+    return res.status(200).json({ message: "Cart item created successfully", response })
+  }
 })
 
 //Delete a single cart item
-router.delete("/:itemId", requireAuth, async (req, res) => {
+router.delete("/:itemId", async (req, res) => {
   const { itemId } = req.params
 
   await prisma.cartItem.delete({
@@ -32,38 +52,74 @@ router.delete("/:itemId", requireAuth, async (req, res) => {
   res.status(200).json({ message: "Item removed" })
 })
 
-//Create a single cart item
-router.post("/create", requireAuth, async (req, res) => {
-  const userId = req.user.sub
+//Increase quantity of a single cart item
+router.patch("/increase/:bookId", async (req, res) => {
+  const { bookId } = req.params
+  const user = await validateUser(req)
+  if (!user) return res.status(401).json({ error: "Unauthorized" })
 
-  //Create of fetch cart of current user
-  const cart = await prisma.cart.upsert({
-    where: { userId },
-    create: { userId },
-    update: {}
-  })
+  const cart = await prisma.cart.findUnique({ where: { userId: user.id } })
+
+  if (!cart) {
+    return res.status(404).json({ error: "Cart not found" })
+  }
 
   const existingItem = await prisma.cartItem.findFirst({
-    where: { cartId: cart.id, bookTitle: req.body.bookTitle }
+    where: { cartId: cart.id, id: bookId }
   })
 
-  if (existingItem) {
-    await prisma.cartItem.update({
-      where: { id: existingItem.id },
-      data: { quantity: existingItem.quantity + 1 }
-    })
-  } else {
-    await prisma.cartItem.create({
-      data: {
-        cartId: cart.id,
-        bookTitle: req.body.bookTitle,
-        image: req.body.image,
-        price: req.body.price,
-        quantity: 1
-      }
-    })
+  if (!existingItem) {
+    return res.status(404).json({ error: "Cart item not found" })
   }
-  res.status(201).json({ message: "Item added to cart" })
+
+  const updatedItem = await prisma.cartItem.update({
+    where: { id: existingItem.id },
+    data: { quantity: { increment: 1 } }
+  })
+
+  res.status(200).json({ message: "Cart item quantity increased", updatedItem })
+})
+
+//Decrease quantity of a single cart item
+router.patch("/decrease/:bookId", async (req, res) => {
+  const { bookId } = req.params
+
+  const user = await validateUser(req)
+  if (!user) return res.status(401).json({ error: "Unauthorized" })
+
+  const cart = await prisma.cart.findUnique({ where: { userId: user.id } })
+  if (!cart) return res.status(404).json({ error: "Cart not found" })
+
+  const existingItem = await prisma.cartItem.findFirst({
+    where: { cartId: cart.id, id: bookId }
+  })
+
+  if (!existingItem) return res.status(404).json({ error: "Cart item not found" })
+  if (existingItem.quantity <= 1) {
+    return res.status(400).json({ error: "Quantity cannot be less than 1" })
+  }
+
+  const updatedItem = await prisma.cartItem.update({
+    where: { id: existingItem.id },
+    data: { quantity: { decrement: 1 } }
+  })
+
+  res.status(200).json({ message: "Cart item quantity decreased", updatedItem })
+})
+
+//Update a single cart item
+router.put("/:itemId", async (req, res) => {
+  const { itemId } = req.params
+  const { quantity } = req.body
+
+  if (quantity < 1) return res.status(400).json({ error: "Quantity must be at least 1" })
+
+  const updateItem = await prisma.cartItem.update({
+    where: { id: itemId },
+    data: { quantity }
+  })
+
+  res.status(200).json({ message: "Cart item updated successfully", updateItem })
 })
 
 export default router
