786dd9da63e9ab6da607c671adf587fb2c93b2f7
Pranay Dutta <rick15176@gmail.com>
2026-04-08 21:15:12 +0530

    refactor: store books in db to handle rate limiting

--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -1,11 +1,10 @@
-import { Request, Router } from "express";
-import adviceHowToBooks from "../cache/advice_how_to_and_miscellaneous.json";
-import childrenMiddleGradeHardCoverBooks from "../cache/childrens-middle-grade-hard-cover.json";
-import hardCoverNonFictionBooks from "../cache/hardcover-nonfiction.json";
-import seriesBooks from "../cache/series-books.json";
-import youngAdultHardcoverBooks from "../cache/young-adult-hardcover.json";
+import { List } from "@/interfaces/List"
+import createNytClient from "@/services/api-client/nytClient"
+import { createBookList, createBooks } from "@/services/nytDatabaseService"
+import { Request, Router } from "express"
+import { prisma } from "@lib/prisma"
 
-const router = Router();
+const router = Router()
 
 type Categories =
   | "combined-print-and-e-book-fiction"
@@ -25,29 +24,48 @@ type Categories =
   | "graphic-books-and-manga"
   | "mass-market-monthly"
   | "middle-grade-paperback-monthly"
-  | "young-adult-paperback-monthly";
-
-const bookMap = new Map<Categories, any>([
-  ["advice-how-to-and-miscellaneous", adviceHowToBooks],
-  ["childrens-middle-grade-hardcover", childrenMiddleGradeHardCoverBooks],
-  ["series-books", seriesBooks],
-  ["young-adult-hardcover", youngAdultHardcoverBooks],
-  ["hardcover-nonfiction", hardCoverNonFictionBooks],
-]);
+  | "young-adult-paperback-monthly"
 
 router.get("/", (_, res) => {
-  res.status(200).json("Books route is working");
-});
+  res.status(200).json("Books route is working")
+})
 
-router.get("/:category", (req: Request<{ category: Categories }>, res) => {
-  const { category } = req.params;
+router.get("/:category", async (req: Request<{ category: Categories }>, res) => {
+  const { category } = req.params
+  const data = await prisma.nytBookList.findFirst({
+    where: { list_name_encoded: category },
+    include: { books: true }
+  })
 
-  if (!bookMap.has(category)) {
-    return res.status(400).json({ error: "Invalid category" });
+  //If data exists in the database, return it.
+  if (data && data.books.length !== 0) {
+    return res.status(200).json(data)
+  } else {
+    //else fetch from the API, store in the database, and return it.
+    const client = createNytClient<List>(`current/${category}.json`)
+    const data = await client.getAll()
+
+    if (data.books.length === 0) {
+      return res.status(404).json({ message: "No books found for this category" })
+    }
+
+    try {
+      // Store the fetched data in the database
+      await createBookList(data)
+      await createBooks(data)
+
+      // Fetch the data from the database
+      const list = await prisma.nytBookList.findFirst({
+        where: { list_name_encoded: category },
+        include: { books: true }
+      })
+
+      //then return the data to the client
+      res.status(200).json(data)
+    } catch (error) {
+      return res.status(500).json({ message: "Error occurred while storing books in the database" })
+    }
   }
+})
 
-  const data = bookMap.get(category) || [];
-  res.status(200).json(data);
-});
-
-export default router;
+export default router
